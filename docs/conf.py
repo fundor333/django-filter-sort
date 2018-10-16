e154090b78d68e27cc8ef70ac4479b84963e2eb0
Matteo <matteo.scarpa@alilaguna.it>
2018-10-16 15:55:22 +0200

    Update Theme

--- docs/conf.py
+++ docs/conf.py
@@ -70,6 +70,8 @@ pygments_style = "monokai"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_theme = "classic"
+
 html_css_files = ["monokai.css"]
 
 
