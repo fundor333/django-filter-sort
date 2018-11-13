b56e1be1ff598bf9273254e6e1f9eead8a0dd17f
Matteo <fundor333@gmail.com>
2018-11-13 11:21:23 +0100

    Some code for sorting

--- filter_sorter/views/advanceviews.py
+++ filter_sorter/views/advanceviews.py
@@ -1,15 +1,17 @@
 from django.conf import settings
-from django.views.generic.list import  BaseListView
+from django.views.generic.list import BaseListView
+
 
 class AdvanceListView(BaseListView):
-    template_name_suffix = "_list" 
+    template_name_suffix = "_list"
 
     paginate_by = getattr(settings, "DJANGO_FILTER_SORT_PAGINATE_BY", None)
     sort_kwarg = getattr(settings, "DJANGO_FILTER_SORT_SORT_KWARG", "sort")
 
     def get_ordering(self):
         """Return the field or fields to use for ordering the queryset."""
-        if self.sort_kwarg in self.kwargs:
-            return self.kwargs[self.sort_kwarg]
+        order = self.request.GET.get(self.sort_kwarg)
+        if order:
+            return order
         else:
-            self.ordering
+            return self.ordering
